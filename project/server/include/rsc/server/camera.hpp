d6699de96d95bc9548608318f95558803ce18edc
locatw <dummy@mail.com>
2014-09-16 10:10:18 +0900

    create camera interface

---
+++ project/server/include/rsc/server/camera.hpp
@@ -0,0 +1,31 @@
+#ifndef INC_RSC_SERVER_CAMERA_HPP
+#define INC_RSC_SERVER_CAMERA_HPP
+
+#include <memory>
+
+namespace rsc {
+namespace server {
+
+class camera_frame;
+
+class camera
+{
+public:
+	camera() = default;
+
+	virtual ~camera() = default;
+
+	virtual bool open() = 0;
+
+	virtual size_t getFrameSize() const = 0;
+
+	virtual std::shared_ptr<camera_frame> retrieve() = 0;
+
+private:
+	camera(const camera&) = default;
+	camera& operator=(const camera&) = default;
+};
+
+}}
+
+#endif
