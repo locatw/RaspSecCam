9f4df52b474f293829a589d2c10f496209c9ddf0
locatw <dummy@mail.com>
2014-09-17 20:34:48 +0900

    create concurrent queue class implements minimum member functions

---
+++ project/server/include/rsc/server/concurrent_queue.hpp
@@ -0,0 +1,94 @@
+#ifndef INC_RSC_SERVER_CONCURRENT_QUEUE_HPP
+#define INC_RSC_SERVER_CONCURRENT_QUEUE_HPP
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+namespace rsc {
+namespace server {
+
+/**
+ * This class is a queue with concurrency.
+ * 
+ * @attention Not all member functions and non member functions are implemented.
+ */
+template<class T, class Container = std::deque<T>>
+class concurrent_queue
+{
+private:
+	typedef std::queue<T, Container> queue_type;
+
+public:
+	typedef typename queue_type::value_type value_type;
+	typedef typename queue_type::reference reference;
+	typedef typename queue_type::const_reference const_reference;
+	typedef typename queue_type::size_type size_type;
+	typedef	Container container_type;
+
+public:
+	explicit concurrent_queue(const Container& other = Container())
+		: mutex_(), condition_(), queue_(other)
+	{}
+
+	bool empty() const
+	{
+		return queue_.empty();
+	}
+
+	size_type size() const
+	{
+		std::lock_guard<std::mutex> lock(mutex_);
+		return queue_.size();
+	}
+
+	value_type& front()
+	{
+		return const_cast<value_type&>(static_cast<const concurrent_queue*>(this)->front());
+	}
+
+	const value_type& front() const
+	{
+		std::unique_lock<std::mutex> lock(mutex_);
+
+		while (queue_.empty()) {
+			condition_.wait(lock);
+		}
+
+		const T& value = queue_.front();
+
+		lock.unlock();
+
+		return value;
+	}
+
+	void push(const value_type& x)
+	{
+		std::unique_lock<std::mutex> lock(mutex_);
+
+		queue_.push(x);
+
+		lock.unlock();
+		condition_.notify_one();
+	}
+
+	void pop()
+	{
+		std::unique_lock<std::mutex> lock(mutex_);
+
+		while (queue_.empty()) {
+			condition_.wait(lock);
+		}
+
+		queue_.pop();
+	}
+
+private:
+	mutable std::mutex mutex_;
+	mutable std::condition_variable condition_;
+	std::queue<T, Container> queue_;
+};
+
+}}
+
+#endif
---
+++ project/server/test/rsc/server/concurrent_queue_test.cpp
@@ -0,0 +1,71 @@
+#include <gtest/gtest.h>
+#include "rsc/server/concurrent_queue.hpp"
+
+using namespace rsc::server;
+
+TEST(concurrent_queue_test, ConstructDefault)
+{
+	concurrent_queue<int> queue;
+
+	EXPECT_EQ(0, queue.size());
+}
+
+TEST(concurrent_queue_test, EmptyReturnsTrueIfQueueIsEmpty)
+{
+	concurrent_queue<int> queue;
+
+	EXPECT_EQ(0, queue.size());
+	EXPECT_TRUE(queue.empty());
+}
+
+TEST(concurrent_queue_test, SizeIncreasedAfterPushingValue)
+{
+	concurrent_queue<int> queue;
+	
+	EXPECT_EQ(0, queue.size());
+
+	queue.push(1);
+
+	EXPECT_EQ(1, queue.size());
+}
+
+TEST(concurrent_queue_test, SizeDecreasedAfterPopingValue)
+{
+	concurrent_queue<int> queue;
+	queue.push(1);
+
+	EXPECT_EQ(1, queue.size());
+
+	queue.pop();
+
+	EXPECT_EQ(0, queue.size());
+}
+
+TEST(concurrent_queue_test, FrontReturnsFirstPushedValue)
+{
+	concurrent_queue<int> queue;
+
+	queue.push(1);
+	queue.push(2);
+
+	EXPECT_EQ(1, queue.front());
+}
+
+TEST(concurrent_queue_test, SizeNotChangedAfterCallingFront)
+{
+	concurrent_queue<int> queue;
+	queue.push(1);
+	queue.front();
+
+	EXPECT_EQ(1, queue.size());
+}
+
+TEST(concurrent_queue_test, ConstFrontReturnsFirstPushedValue)
+{
+	concurrent_queue<int> queue;
+
+	queue.push(1);
+	queue.push(2);
+
+	EXPECT_EQ(1, static_cast<const concurrent_queue<int>*>(&queue)->front());
+}
