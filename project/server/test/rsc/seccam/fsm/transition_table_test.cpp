e9b32aaea06cf9f42a73738282faf7bc4cc302d9
locatw <dummy@mail.com>
2014-10-06 16:30:30 +0900

    add tests for transition_table::lookup_next_state

--- project/server/test/rsc/seccam/fsm/transition_table_test.cpp
+++ project/server/test/rsc/seccam/fsm/transition_table_test.cpp
@@ -37,3 +37,24 @@ TEST(transition_table_test, ConstructTwoEntryTable)
 
 	EXPECT_EQ(2, table.size());
 }
+
+TEST(transition_table_test, LookupNextStateAndFound)
+{
+	transition_table_type table = {
+		transition_table_entry_type(state_id::state1, event::event1, state_id::state1),
+		transition_table_entry_type(state_id::state1, event::event2, state_id::state2)
+	};
+
+	EXPECT_EQ(state_id::state1, *table.lookup_next_state(state_id::state1, event::event1));
+	EXPECT_EQ(state_id::state2, *table.lookup_next_state(state_id::state1, event::event2));
+}
+
+TEST(transition_table_test, LookupNextStateAndNotFound)
+{
+	transition_table_type table = {
+		transition_table_entry_type(state_id::state1, event::event1, state_id::state1),
+	};
+
+	EXPECT_FALSE(table.lookup_next_state(state_id::state1, event::event2));
+	EXPECT_FALSE(table.lookup_next_state(state_id::state2, event::event1));
+}
